a3316b80cd3df853808e75fae83e6a84ca12f2f3
Dima Rogov <ds.rogv@gmail.com>
2015-09-02 00:29:30 +0300

    Complete User Sign Up

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -21,6 +21,13 @@ describe "User Pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_content('error') }
+        it { should have_title(full_title('Sign Up')) }
+      end
     end
 
     describe "with valid information" do
@@ -33,6 +40,14 @@ describe "User Pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: "user@example.com") }
+
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_title(user.name) }
       end
     end
 
