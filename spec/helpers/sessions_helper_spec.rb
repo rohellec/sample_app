0bd9efffb507cdd00bdc15a4f0f59c64b67b9b94
Dima Rogov <ds.rogv@gmail.com>
2015-10-12 23:32:32 +0300

    Add cookies test in session helper spec and update tests for links inside authentication spec

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -11,7 +11,16 @@ describe SessionsHelper do
   end
 
   describe "current_user returns nil when remember_digest is wrong" do
-    before { user.update_attribute(:remember_digest, User.digest(User.new_token))}
+    before { user.update_attribute(:remember_digest, User.digest(User.new_token)) }
     specify { expect(current_user).to be_nil }
+  end
+
+  describe "when user is remembered cookies should not be nil" do
+    specify { expect(cookies[:remember_token]).not_to be_nil }
+  end
+
+  describe "when user is forgotten cookies should be nil" do
+    before  { forget(user) }
+    specify { expect(cookies[:remember_token]).to be_nil }
   end
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -45,6 +45,7 @@ describe 'Authentication' do
         it { should have_link('Sign in', href: signin_path) }
         it { should_not have_link('Sign out', href: signout_path) }
         it { should_not have_link('Profile',  href: user_path(user)) }
+        it { should_not have_link('Settings', href: edit_user_path(user)) }
         it { should_not have_link('Users', href: users_path) }
 
         describe 'and then signed out from another window' do
@@ -54,6 +55,7 @@ describe 'Authentication' do
           it { should have_link('Sign in', href: signin_path) }
           it { should_not have_link('Sign out', href: signout_path) }
           it { should_not have_link('Profile',  href: user_path(user)) }
+          it { should_not have_link('Settings', href: edit_user_path(user)) }
           it { should_not have_link('Users', href: users_path) }
         end
       end
