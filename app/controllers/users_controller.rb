3243a88bf0238751a4fd5ada29bb5af0e0fc10f7
Dima Rogov <ds.rogv@gmail.com>
2015-11-04 17:17:13 +0300

    Update before_action filter for signed-in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-  before_action :not_signed_in,  only: [:new, :create]
+  before_action :non_signed_in_user, only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 10)
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user.admin?
     end
 
-    def not_signed_in
-      redirect_to users_url if signed_in?
+    def non_signed_in_user
+      redirect_to(root_url) if signed_in?
     end
 end
