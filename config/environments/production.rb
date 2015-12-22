95417ec4efcc495264e95d3cc8ec1a887ec79084
Dima Rogov <ds.rogv@gmail.com>
2015-12-22 23:57:45 +0300

    Fix production config syntax bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,9 +83,9 @@ Rails.application.configure do
     address: "smtp.sendgrid.net",
     port: "587",
     authentication: :plain,
-    user_name: ENV["SENDGRID_USERNAME"]
-    password:  ENV["SENDGRID_PASSWORD"]
-    domain: "heroku.com"
+    user_name: ENV["SENDGRID_USERNAME"],
+    password:  ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
     enable_starttls_auto: true
   }
 end
